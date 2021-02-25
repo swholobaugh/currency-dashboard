3898e4226bad7e6ec10b68e3c9e77100dfaea797
Seth Holobaugh <seth.holobaugh@gmail.com>
2021-02-25 17:39:29 -0600

    Fixed appbar path

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,7 +4,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Menu from '@material-ui/core/Menu';
 import MenuIcon from '@material-ui/icons/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import AppBar from '@material-ui/core/Appbar';
+import AppBar from '@material-ui/core/AppBar';
 
 const useStyles = makeStyles(theme => ({
   navbarRoot: {
